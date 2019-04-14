305b96b3e5bc4fc8d8f4d19b8107a5b3d7790af1
Poyraz <poyrazylmaz@gmail.com>
2019-04-14 21:25:44 +0300

    use papaParser to fetch and parse csv data and transform into products array...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import * as Papa from 'papaparse';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
+/**
+ * Transform data array to data object
+ * where object properties come from properties array
+ */
+const transformData = (properties, data) => (
+  data
+    .map(values => properties
+      .reduce((object, property, propertyIndex) => {
+        object[property] = values[propertyIndex];
+        return object;
+      }, {})
+    )
+)
 
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  /**
+   * In this effect function we fetch products.csv and transform into list of product objects
+   * This effect will run only one time
+   */
+  useEffect(() => {
+    Papa.parse('/products.csv', {
+      download: true,
+      skipEmptyLines: true,
+      complete: (results) => {
+        const data = transformData(results.data[0], results.data.slice(1));
+        setProducts(data);
+        setLoading(false);
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      Is loading: {loading ? 'loading' : 'not loading'}
+      <br></br>
+      Product count: {products.length}
+    </div>
+  );
+};
 
 export default App;
