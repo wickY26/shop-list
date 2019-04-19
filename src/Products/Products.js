89ad4b052591cae67324bd0544aebe3dcec12b90
POYRAZ YILMAZ <poyrazylmaz@gmail.com>
2019-04-19 18:32:37 +0300

    Products tests...

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -3,10 +3,10 @@ import { Typography } from '@material-ui/core';
 import { FixedSizeList } from 'react-window';
 import Product from './Product/Product';
 
-const generateContent = (products, index, style) => {
+export const generateContent = (products = [], productIndex, style) => {
   let content;
-  if (index < products.length) {
-    content = <Product product={products[index]} />;
+  if (productIndex < products.length) {
+    content = <Product product={products[productIndex]} />;
   } else {
     content = <Typography variant="h4" align="center">Loading...</Typography>;
   }
